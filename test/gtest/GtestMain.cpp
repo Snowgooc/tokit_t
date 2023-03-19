5c30d6858bab26c71f107acc4b45fee46ff77547
896937848@qq.com <896937848@qq.com>
2023-03-19 18:36:19 +0800

    add googletest

---
+++ test/gtest/GtestMain.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "gtest/gtest.h"
+
+
+
+TEST(GTEST_temp, _temp)
+{
+    printf("Run _temp\n");
+    ASSERT_TRUE(1);
+}
+
+
+
+
+
+
+
+
+
